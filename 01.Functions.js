6e4c32f7400ec9b0079e0d28f07d9b1da4953097
jean.boulet <professor.boulet@gmail.com>
2018-09-10 09:22:39 -0400

    added taxes to the payCheck function;

--- 01.Functions.js
+++ 01.Functions.js
@@ -1,9 +1,12 @@
 
 
-var payCheck = function(hourlyWage, hoursWorked){
+var payCheck = (hourlyWage, hoursWorked) => {
     var income = hourlyWage * hoursWorked;
-    return income;
+    var tax = income * .35;
+    return {income,tax};
 }
+
+
 var result1 = payCheck(10,35);
 
 console.log({output1: result1 });
