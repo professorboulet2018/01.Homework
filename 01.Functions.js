d56b84ba3577ba30f551d69d7159e44cbe1e653d
jean.boulet <professor.boulet@gmail.com>
2018-09-10 09:34:51 -0400

    added a global paycheck function to handle overtime

--- 01.Functions.js
+++ 01.Functions.js
@@ -7,6 +7,27 @@ var payCheck = (hourlyWage, hoursWorked) => {
 }
 
 
-var result1 = payCheck(10,35);
+function gPayCheck(hourlyWage,hoursWorked) {
+    var income = hourlyWage * hoursWorked;
+    var overtimeStatus = false;
+
+    if (hoursWorked > 40){
+        overtimeStatus = true;
+    }
+    return {
+        hourlyWage, hoursWorked,
+        income, overtimeStatus
+    }
+}
+
+var result1 = gPayCheck(10,42);
+var result2 = gPayCheck(10,41);
+var result3 = gPayCheck(10,40);
+var result4 = gPayCheck(10,39);
+var result5 = gPayCheck(10,38);
 
 console.log({output1: result1 });
+console.log({output2: result2 });
+console.log({output3: result3 });
+console.log({output4: result4 });
+console.log({output5: result5 });
