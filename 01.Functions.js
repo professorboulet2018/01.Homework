e74dc0fc272756663bb48fba7222752d9c15a322
jean.boulet <professor.boulet@gmail.com>
2018-09-10 09:16:21 -0400

    created a simple function

---
+++ 01.Functions.js
@@ -0,0 +1,9 @@
+
+
+var payCheck = function(hourlyWage, hoursWorked){
+    var income = hourlyWage * hoursWorked;
+    return income;
+}
+var result1 = payCheck(10,35);
+
+console.log({output1: result1 });
